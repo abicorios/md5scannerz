2da06d717eaa8cba5c4c8590708048ce2830ec22
Yaroslav Moroz <yaroslav.moroz91@outlook.com>
2018-07-22 20:25:36 +0300

    fix release

--- release.bat
+++ release.bat
@@ -2,10 +2,6 @@ start /wait cmd /c "make.bat"
 copy app.html md5scanerz\app /y
 copy app.js md5scanerz\src /y
 if not exist md5scanerz\resources\app mkdir md5scanerz\resources\app
-copy longpathgo.exe md5scanerz\resources\app /y
-copy longpathgo.exe md5scanerz /y
-copy md5scannerzrb.exe md5scanerz\resources\app /y
-copy md5scannerzrb.exe md5scanerz /y
 copy package.json md5scanerz\package.json /y
 xcopy 7z1805-extra md5scanerz\7z1805-extra\ /s /y
 xcopy 7z1805-extra md5scanerz\resources\app\7z1805-extra\ /s /y
