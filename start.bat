bdd7de36d32f13a20cf192c73099e604873b18b1
Yaroslav Moroz <yaroslav.moroz91@outlook.com>
2018-07-22 19:03:15 +0300

    rm rb

--- start.bat
+++ start.bat
@@ -4,8 +4,6 @@ copy app.js md5scanerz\src /y
 if not exist md5scanerz\resources\app mkdir md5scanerz\resources\app
 copy md5scannerzgo.exe md5scanerz\resources\app /y
 copy md5scannerzgo.exe md5scanerz /y
-copy md5scannerzrb.exe md5scanerz\resources\app /y
-copy md5scannerzrb.exe md5scanerz /y
 copy package.json md5scanerz\package.json /y
 xcopy 7z1805-extra md5scanerz\7z1805-extra\ /s /y
 xcopy 7z1805-extra md5scanerz\resources\app\7z1805-extra\ /s /y
