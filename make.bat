ed1a527665f313296b04e6f5fb95f9a85624adb0
abicorios <yaroslav.moroz91@gmail.com>
2018-07-29 12:54:10 -0700

    fix make

--- make.bat
+++ make.bat
@@ -17,7 +17,8 @@ if not exist compileIfChanged start /wait cmd /c git clone https://github.com/ab
 if not exist compileIfChanged.exe start /wait cmd /c go build compileIfChanged\compileIfChanged.go
 if not exist md5scanerz start /wait cmd /c "git clone https://github.com/szwacz/electron-boilerplate.git md5scanerz && cd md5scanerz && npm install"
 start /wait cmd /c ruby date.rb
-if not exist md5scannerzgo start /wait cmd /c git clone https://github.com/abicorios/md5scannerzgo
+where node
+if %errorlevel%==0 if not exist md5scannerzgo start /wait cmd /c git clone https://github.com/abicorios/md5scannerzgo
 copy md5scannerzgo\md5scannerzgo.go
 if not exist md5scannerzgo.exe start /wait cmd /c go build md5scannerzgo.go
 compileIfChanged.exe md5scannerzgo.go md5scannerzgo.exe go build md5scannerzgo.go
