c03d050cb6601436299c8f63f3d5434f76365cb2
abicorios <yaroslav.moroz91@gmail.com>
2018-08-21 15:21:16 -0700

    merge installs of choco and other tools

--- 0adminInstallChoco.bat
+++
@@ -1,3 +0,0 @@
-where choco /q
-if not %errorlevel%==0 (start /wait cmd /c @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin")
-exit
--- 1adminInstallTools.bat
+++ 1adminInstallTools.bat
@@ -1,3 +1,6 @@
+where choco /q
+if not %errorlevel%==0 (start /wait cmd /c @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin")
+call C:\ProgramData\chocolatey\bin\refreshenv
 where 7z /q
 if not %errorlevel%==0 (choco install 7zip --x86 -y)
 where git /q
@@ -10,4 +13,6 @@ where node /q
 if not %errorlevel%==0 (choco install nodejs --x86 -y)
 where vim /q
 if not %errorlevel%==0 (start https://ftp.nluug.nl/pub/vim/pc/gvim81.exe)
-exit
+@set /p a=It is a pause, press Enter after an installation of vim
+@set a=
+call refreshenv
