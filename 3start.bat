9191fa85c0cc6240c75ff502338556c50deb9d26
abicorios <yaroslav.moroz91@gmail.com>
2018-08-21 16:53:08 -0700

    fix dir name

--- 3start.bat
+++ 3start.bat
@@ -1,10 +1,10 @@
-start /wait cmd /c 2make.bat
-copy app.html md5scanerz\app /y
-copy app.js md5scanerz\src /y
-if not exist md5scanerz\resources\app mkdir md5scanerz\resources\app
-copy md5scannerzgo.exe md5scanerz\resources\app /y
-copy md5scannerzgo.exe md5scanerz /y
-copy package.json md5scanerz\package.json /y
-xcopy 7z1805-extra md5scanerz\7z1805-extra\ /s /y
-xcopy 7z1805-extra md5scanerz\resources\app\7z1805-extra\ /s /y
-start cmd /c "cd md5scanerz && npm start"
+2make.bat
+copy app.html md5scannerz\app /y
+copy app.js md5scannerz\src /y
+if not exist md5scannerz\resources\app mkdir md5scannerz\resources\app
+copy md5scannerzgo.exe md5scannerz\resources\app /y
+copy md5scannerzgo.exe md5scannerz /y
+copy package.json md5scannerz\package.json /y
+xcopy 7z1805-extra md5scannerz\7z1805-extra\ /s /y
+xcopy 7z1805-extra md5scannerz\resources\app\7z1805-extra\ /s /y
+start cmd /c "cd md5scannerz && npm start"
--- 4release.bat
+++ 4release.bat
@@ -1,9 +1,9 @@
-start /wait cmd /c "make.bat"
-copy app.html md5scanerz\app /y
-copy app.js md5scanerz\src /y
-if not exist md5scanerz\resources\app mkdir md5scanerz\resources\app
-copy package.json md5scanerz\package.json /y
-xcopy 7z1805-extra md5scanerz\7z1805-extra\ /s /y
-xcopy 7z1805-extra md5scanerz\resources\app\7z1805-extra\ /s /y
-start /wait cmd /c "cd md5scanerz && npm run release"
-copy  md5scanerz\dist\md5scannerz* /y
+make.bat
+copy app.html md5scannerz\app /y
+copy app.js md5scannerz\src /y
+if not exist md5scannerz\resources\app mkdir md5scannerz\resources\app
+copy package.json md5scannerz\package.json /y
+xcopy 7z1805-extra md5scannerz\7z1805-extra\ /s /y
+xcopy 7z1805-extra md5scannerz\resources\app\7z1805-extra\ /s /y
+start /wait cmd /c "cd md5scannerz && npm run release"
+copy  md5scannerz\dist\md5scannerz* /y
