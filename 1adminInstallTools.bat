0699ded34d277de2def11b89c636f642caa33992
abicorios <yaroslav.moroz91@gmail.com>
2018-08-22 14:57:07 -0700

    add flags

--- 1adminInstallTools.bat
+++ 1adminInstallTools.bat
@@ -1,18 +1,22 @@
 where choco /q
-if not %errorlevel%==0 (start /wait cmd /c @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin")
-call C:\ProgramData\chocolatey\bin\refreshenv
+if not %errorlevel%==0 (@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+call C:\ProgramData\chocolatey\bin\refreshenv)
+set f=0
 where 7z /q
-if not %errorlevel%==0 (choco install 7zip --x86 -y)
+if not %errorlevel%==0 (choco install 7zip --x86 -y
+set f=1)
 where git /q
-if not %errorlevel%==0 (choco install git --x86 -y)
+if not %errorlevel%==0 (choco install git --x86 -y
+set f=1)
 where go /q
-if not %errorlevel%==0 (choco install golang --x86 -y)
+if not %errorlevel%==0 (choco install golang --x86 -y
+set f=1)
 where ruby /q
-if not %errorlevel%==0 (choco install ruby --x86 -y)
+if not %errorlevel%==0 (choco install ruby --x86 -y
+set f=1)
 where node /q
-if not %errorlevel%==0 (choco install nodejs --x86 -y)
-where vim /q
-if not %errorlevel%==0 (start https://ftp.nluug.nl/pub/vim/pc/gvim81.exe
-@set /p a=It is a pause, press Enter after an installation of vim
-@set a=)
-call refreshenv
+if not %errorlevel%==0 (choco install nodejs --x86 -y
+set f=1)
+rem if not %errorlevel%==0 start https://ftp.nluug.nl/pub/vim/pc/gvim81.exe
+if %f% equ 1 call refreshenv
+set f=
